b66499125cbc07e8f70c3a106693551fac175fbf
Huaesthetica <46458667+huaesthetica63@users.noreply.github.com>
2022-06-28 02:02:46 +0400

    first

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(408, 344);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(429, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "upload photo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(446, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(57, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "100";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(443, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "binarization";
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(429, 316);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(74, 40);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Find!";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Hough);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 389);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Hough Transformation";
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap bitmap0; //исходное изображение запоминаем в отдельную переменную
+        private int sizeLine = 3;//размер линии для выделения найденного контура
+        byte gray(Color col)//формула перевода цветного пикселя в черно-белый
+        {
+            return (byte)(0.3*col.R + 0.59*col.G + 0.11*col.B);
+        }
+        public Form1()//конструктор по умолчанию задает фильтр для изображения
+        {
+            InitializeComponent();
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "JPEG files (*.jpg *.jpeg *.jfif)|*.jpg;*.jpeg;*.jfif"+
+                "|PNG files (*.png)|*.png"+
+                "|BMP files (*.bmp)|*.bmp"+
+                "|TIF files (*.tif *.tiff)|*.tif;*.tiff"+
+                "|GIF files (*.gif)|*.gif"+
+                "|All files (*.jpg *.jpeg *.jfif *.png *.bmp *.tif *.tiff *.gif)|*.jpg;*.jpeg;*.jfif;*.png;*.bmp;*.tif;*.tiff;*.gif";
+        }
+        
+        
+        private void button1_Click(object sender, EventArgs e)//обязательно загруженную картинку переводим в чб
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            Bitmap bitmap = new Bitmap(openFileDialog1.FileName);
+            bitmap0 = new Bitmap(bitmap);
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for(int j = 0; j < bitmap.Height; j++)
+                {
+                    Color pix = bitmap.GetPixel(i, j);
+                    Color newpix = Color.FromArgb(pix.A, gray(pix), gray(pix), gray(pix));
+                    bitmap.SetPixel(i, j, newpix);
+                }
+            }
+            pictureBox1.Image = bitmap;
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            button3.Enabled = true;
+        }
+        public Bitmap binarization(Bitmap bitm, int val)//бинаризация картинки с порогом val - получим картинку только с двумя цветами
+        {
+            Bitmap bitmap = new Bitmap(bitm);
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    var pix = bitmap.GetPixel(i, j);
+                    int r = pix.R;
+                    int g = pix.G;
+                    int b = pix.B;
+                    if (r < val)
+                        r = 0;
+                    else
+                        r = 255;
+                    if (g < val)
+                        g = 0;
+                    else
+                        g = 255;
+                    if (b < val)
+                        b = 0;
+                    else
+                        b = 255;
+                    Color newpix = Color.FromArgb(bitmap.GetPixel(i, j).A, r, g, b);
+                    bitmap.SetPixel(i, j, newpix);
+                }
+            }
+            return bitmap;
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+
+            pictureBox1.Width = this.Width - 350;
+            pictureBox1.Height = this.Height - 70;
+        }
+        double getRo(double fi, int x, int y)//получаем радиус перпендикуляра
+        {
+            return x * Math.Cos(fi) + y * Math.Sin(fi);//х и у - координата точки на картинке из котрой проводим пучок прямых
+        }
+        double radFromDegree(int degree)//радиан из градуса для функций тригонометрии
+        {
+            return degree * (Math.PI / 180.0);
+        }
+        struct fiRo//структура одной уникальной прямой
+        {
+            public int fidegree;//фи
+            public int ro;//радиус перпендикуляра к прямой
+            public fiRo(int d, int r)
+            {
+                fidegree = d;//конструктор по умолчанию
+                ro = r;
+            }
+        };
+
+        private void Hough(object sender, EventArgs e)
+        {
+            try
+            {
+                Bitmap binarize = new Bitmap(bitmap0);
+                binarize = binarization(bitmap0, Int32.Parse(textBox1.Text));
+                Bitmap bmp = new Bitmap(binarize);//копируем оригинал картинки
+                Dictionary<fiRo, int> map = new Dictionary<fiRo, int>();//словарь - ключу соответствует уникальная прямая из точки, значение по ключу - количество таких прмых полученных из всех точек
+                for (int i = 0; i < bmp.Width; i++)
+                {
+                    for (int j = 0; j < bmp.Height; j++)//обходим все пиксели абсолютно
+                    {
+                        if (bmp.GetPixel(i, j).R == 0)//если это пиксель черный - это точка для рассмотрения
+                        {
+                            for (int fiDegree = 0; fiDegree < 180; fiDegree++)//обход половины полного оборота ввиду того, что повторяться два раза нет смысла
+                            {
+                                double fi = radFromDegree(fiDegree);//перевод градуса в радиан для удобства
+                                double ro = getRo(fi, i, j);//получение длины перпендикуляра
+                                fiRo temp = new fiRo(fiDegree, (int)ro);//уникальная прямая 
+                                //Console.WriteLine(fiDegree.ToString() + "  " + ((int)(ro)).ToString());
+                                if (!map.ContainsKey(temp))//если такой прямой еще не было
+                                {
+                                    map.Add(temp, 0);//добавляем ее
+                                }
+                                map[temp]++;//увеличиваем частоту встречи такой прямой на единицу
+                            }
+                        }
+                    }
+                }
+                int maxval = map.Values.Max();//максимальное количество одинаковых прмяых
+                var keyOfMax = map.Aggregate((x, y) => x.Value > y.Value ? x : y).Key;//выражение для получения ключа по значению
+                MessageBox.Show("MaxVal: " + maxval.ToString() + "; fi: " + keyOfMax.fidegree + "; ro: " + keyOfMax.ro);//выводим данные этой прямой
+                //рисование линии
+                draw(keyOfMax.fidegree, keyOfMax.ro);
+            }
+            
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error!");
+            }
+        }
+        public void draw(int degree, int ro)
+        {
+            try
+            {
+                
+                Graphics gr = Graphics.FromImage(pictureBox1.Image);
+                
+                int y0 = (int)((ro) / Math.Sin(radFromDegree(degree)));
+                int y1 = (int)((ro - pictureBox1.Image.Width * Math.Cos(radFromDegree(degree)) / Math.Sin(radFromDegree(degree))));
+                gr.DrawLine(new Pen(Color.Red, sizeLine), new Point(0, y0), new Point(pictureBox1.Image.Width, y1));
+                
+                pictureBox1.Refresh();
+                pictureBox1.Update();
+                this.Invalidate();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error!");
+            }
+          
+        }
+    }
+}
